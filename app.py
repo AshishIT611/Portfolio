fc47fd58c51ecde5c5ce6e111b37b5d8acf434ac
Ashish Yadav <ay87896@gmail.com>
2025-05-22 18:38:58 +0530

    new_work

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask,render_template,request
 from flask_mysqldb import MySQL
+from flask_cors import CORS
 app=Flask(__name__)
+CORS(app)
 app.config["MYSQL_HOST"]="localhost"
 app.config["MYSQL_USER"]="root"
 app.config["MYSQL_PASSWORD"]="ashish143@"
